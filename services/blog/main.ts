67b2d2590765a360e2c4e2e5195332636e0170bc
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2025-06-30 08:21:21 +0100

    refactor

--- services/blog/main.ts
+++ services/blog/main.ts
@@ -1,24 +1,25 @@
 import { Elysia } from "elysia";
-import MongoClient from "@learning/connectors/clients/mongo.client";
-import { Schema } from "mongoose";
 import BlogService from "./services/blog.service";
 import { BlogDTO } from "./dto/blog.dto";
 import BlogRepository from "./repositories/blog.repository";
 import BlogController from "./controllers/blog.controller";
 
+const controllers = {
+	blog: async () => {
+		const blogRepo = await BlogRepository.init();
+		const blogService = new BlogService(blogRepo);
+		return new BlogController(blogService);
+	},
+};
+
 const app = new Elysia()
 	.get("/", async () => {
-		const blogRepo = await BlogRepository.init();
-
-		const blogService = new BlogService(blogRepo);
-
-		const blogController = new BlogController(blogService);
-
 		const blog = new BlogDTO();
-
 		blog.title = "Hello World";
 
-		return await blogController.createBlog(blog);
+		return await controllers.blog().then((context) => {
+			return context.createBlog(blog);
+		});
 	})
 	.listen({
 		hostname: "0.0.0.0",
