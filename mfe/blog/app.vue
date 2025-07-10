4cf6e0b18c91aa6d324998b54d16a5d9ca9fd8cb
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2025-07-10 06:53:54 +0100

    setup graphql on microfrontend

--- mfe/blog/app.vue
+++ mfe/blog/app.vue
@@ -2,5 +2,26 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    {{ data }}
+    {{ error }}
   </div>
 </template>
+
+<script setup lang="ts">
+import { gql } from "nuxt-graphql-request/utils";
+
+const { $graphql } = useNuxtApp();
+
+const query = gql`
+  query getBlog {
+    getBlog {
+      title
+    }
+  }
+`;
+
+const { data, error } = await useAsyncData("getblog", async () => {
+  const data = await $graphql.default.request(query);
+  return data.getBlog;
+});
+</script>
--- mfe/blog/nuxt.config.ts
+++ mfe/blog/nuxt.config.ts
@@ -2,6 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  modules: ["nuxt-graphql-request"],
+  build: {
+    transpile: ["nuxt-graphql-request"],
+  },
+  graphql: {
+    /**
+     * An Object of your GraphQL clients
+     */
+    clients: {
+      default: {
+        /**
+         * The client endpoint url
+         */
+        endpoint: "http://100.105.32.95:3000/graphql",
+        /**
+         * Per-client options overrides
+         * See: https://github.com/prisma-labs/graphql-request#passing-more-options-to-fetch
+         */
+        options: {},
+      },
+    },
+  },
+
   devServer: {
     host: "0.0.0.0",
     port: 2000,
